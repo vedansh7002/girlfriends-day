4eb54399f921860ff0ee20bffbbd0db2385b1c19
vedansh7002 <beastboyshubh1999@gmail.com>
2026-07-31 11:26:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-window.onload=()=>{
-setTimeout(()=>{
-document.getElementById("loader").style.display="none";
-},2500);
-}
+window.onload = () => {
+  setTimeout(() => {
+    document.getElementById("loader").style.display = "none";
+  }, 2500);
+};
 
-document.getElementById("startBtn").onclick=function(){
-alert("Part 2 me Love Letter khulega ❤️");
-}
+document.getElementById("startBtn").onclick = function () {
+  alert("Part 2 me Love Letter khulega ❤️");
+};
