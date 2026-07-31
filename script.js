6b0dc113c9ed2dd2f7371e35c2d132222395443d
vedansh7002 <beastboyshubh1999@gmail.com>
2026-07-31 11:24:12 +0530

    Add loading screen and start button functionality

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-// Premium JavaScript Coming Soon
+window.onload=()=>{
+setTimeout(()=>{
+document.getElementById("loader").style.display="none";
+},2500);
+}
+
+document.getElementById("startBtn").onclick=function(){
+alert("Part 2 me Love Letter khulega ❤️");
+}
